ca1a2d6ac75b3fc763a23aff77a1dd91d13135ec
TiagoSRocha <tiagosrochasilva@gmail.com>
2018-12-14 09:44:08 +0000

    Added Brain Logic

---
+++ src/Brain.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Brain implements Runnable{
+
+     private static Brain obj;
+     ArrayList<Device> notificationsPool = new ArrayList<Device>();
+     private Brain() {}
+
+     // Only one thread can execute this at a time
+     public static synchronized Brain getBrain()
+     {
+         if (obj==null) {
+             obj = new Brain();
+            new Thread(new Brain()).start();
+         }
+         return obj;
+     }
+     public void run() {
+         //ir a pool das notificacoes e lidar.
+
+     }
+
+ }
